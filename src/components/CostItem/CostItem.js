340657666a32ecf63e2c5b41c2a724d44f084fa6
Alexander Prokofyev <alexander.prokofyev@skywindgroup.com>
2022-09-03 14:28:18 +0300

    Added composition

--- src/components/CostItem/CostItem.js
+++ src/components/CostItem/CostItem.js
@@ -1,14 +1,15 @@
 import CostDate from "../CostDate/CostDate";
 import './CostItem.css'
+import Card from "../Card/Card";
 
 export default function CostItem() {
     return (
-        <div className="cost-item">
+        <Card className="cost-item">
             <CostDate />
             <div className="cost-item__description">
                 <h2>Холодильник</h2>
                 <div className="cost-item__price">999.99$</div>
             </div>
-        </div>
+        </Card>
     )
 }
--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,12 +1,13 @@
 import CostItem from "../CostItem/CostItem";
 import './Costs.css'
+import Card from "../Card/Card";
 
 export default function Costs() {
     return (
-        <div className="costs">
+        <Card className="costs">
             <CostItem />
             <CostItem />
             <CostItem />
-        </div>
+        </Card>
     )
 }
